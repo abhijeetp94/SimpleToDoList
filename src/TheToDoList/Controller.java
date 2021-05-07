a07f7dd30c8b102ecf942ebc9639a9ad0ac67d1d
Abhijeet Pandey <p.abhijeetp94@gmail.com>
2021-05-07 15:16:25 +0530

    ListView Added

--- src/TheToDoList/Controller.java
+++ src/TheToDoList/Controller.java
@@ -1,4 +1,45 @@
 package TheToDoList;
 
+import TheToDoList.DataView.ToDoItem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Controller {
+    private List<ToDoItem> toDoList;
+    @FXML
+    private ListView<ToDoItem> toDoItemListView;
+
+    public void initialize(){
+        toDoList = new ArrayList<>();
+        toDoList.add(new ToDoItem("Submit Assignment",
+                "Submit the assignment of chapter 4 of maths",
+                LocalDate.of(2021, Month.MAY, 9)));
+        toDoList.add(new ToDoItem("Make Project",
+                "Start Building the project",
+                LocalDate.of(2021, Month.MAY, 11)));
+        toDoList.add(new ToDoItem("Give Contest",
+                "There is a codeforces contest of division 2 you should give it.",
+                LocalDate.of(2021, Month.MAY, 8)));
+        toDoList.add(new ToDoItem("Write Blog",
+                "Write a weblog for the blogspot account of Astrowing MNNIT.",
+                LocalDate.of(2021, Month.MAY, 12)));
+
+        toDoItemListView.getItems().setAll(toDoList);
+        toDoItemListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+    @FXML
+    public void handleSelectedItem(){
+        ToDoItem item = toDoItemListView.getSelectionModel().getSelectedItem();
+        System.out.println("Selected Item is " + item.getTitle());
+    }
+
+
 }
--- src/TheToDoList/DataView/ToDoItem.java
+++ src/TheToDoList/DataView/ToDoItem.java
@@ -12,4 +12,33 @@ public class ToDoItem {
         this.description = description;
         this.deadline = deadline;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
